298882f97d9600749bc603d0beaba30ce063dfa7
Le Thuy Hang <thuyhangle.le@gmail.com>
2018-01-18 01:23:42 +0200

    Fly back

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ window.onload = function() {
     var logo = document.getElementById("logo");
     // TweenLite.to(logo, 2, {ease:Back.easeIn, left:"85%", borderBottomColor:"#74e691", color:"white"});
 
-    var tl = new TimelineMax({repeat:-1})
+    var tl = new TimelineMax({repeat:-1, yoyo: true})
 
     tl.to("#logo", 4, {x:550, ease:Linear.easeNone})
       .to("#logo", 2, {y:-65, ease:Power1.easeInOut, repeat:1, yoyo:true,  borderBottomColor:"#74e691", color:"white"}, 0)
